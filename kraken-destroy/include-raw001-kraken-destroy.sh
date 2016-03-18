97006dbfe5a8fb93b66e3b3d433f03a88239cdc1
Marat G <maratoid@gmail.com>
2016-03-17 18:43:47 -0700

    make sure destroy script pulls state. add a build marker to created cluster names

--- kraken-destroy/include-raw001-kraken-destroy.sh
+++ kraken-destroy/include-raw001-kraken-destroy.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+${WORKSPACE}/bin/kraken-connect.sh \
+  --clustername "${KRAKEN_CLUSTER_NAME}" \
+  --clustertype aws \
+    --dmname "${PIPELET_DOCKERMACHINE}" \
+    --dmshell bash
+    
 ${WORKSPACE}/bin/kraken-down.sh \
   --clustername "${KRAKEN_CLUSTER_NAME}" \
     --clustertype aws \
