0ced9371c40d757751a99f357a4ab8f3f9833ce8
David Watson <dwat@rodin.local>
2016-03-29 11:24:09 -0700

    Fix a typo causing a syntax error.

--- kraken-k8s-density/include-raw001-kraken-k8s-density.sh
+++ kraken-k8s-density/include-raw001-kraken-k8s-density.sh
@@ -3,5 +3,5 @@ pod_densities="${DENSITIES%\"}"
 pod_densities="${pod_densities#\"}"
 
 for i in $pod_densities; do
-  echo "DENSITY=$i > density_$i.property
+  echo "DENSITY=$i" > density_$i.property
 done
