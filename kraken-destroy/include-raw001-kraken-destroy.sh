182180676d3cad6d4fb29d351a7105fea89ac60e
David Watson <davidewatson@gmail.com>
2016-04-04 11:58:16 -0700

    Add back ability to skip destroy-cluster step of jobs (PIP-52).

--- kraken-destroy/include-raw001-kraken-destroy.sh
+++ kraken-destroy/include-raw001-kraken-destroy.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
-${WORKSPACE}/bin/kraken-connect.sh \
-  --clustername "${KRAKEN_CLUSTER_NAME}" \
-  --clustertype aws \
-    --dmname "${PIPELET_DOCKERMACHINE}" \
-    --dmshell bash
-    
-${WORKSPACE}/bin/kraken-down.sh \
-  --clustername "${KRAKEN_CLUSTER_NAME}" \
+
+if [ "${DESTROY_CLUSTER}" = true ]; then
+  ${WORKSPACE}/bin/kraken-connect.sh \
+    --clustername "${KRAKEN_CLUSTER_NAME}" \
     --clustertype aws \
     --dmname "${PIPELET_DOCKERMACHINE}" \
     --dmshell bash
+ 
+  ${WORKSPACE}/bin/kraken-down.sh \
+    --clustername "${KRAKEN_CLUSTER_NAME}" \
+    --clustertype aws \
+    --dmname "${PIPELET_DOCKERMACHINE}" \
+    --dmshell bash
+fi
