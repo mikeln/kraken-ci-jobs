249b6c679eb45310a6ba01f405ab2160077eeceb
David Watson <davidewatson@gmail.com>
2016-05-19 10:14:23 -0700

    The CopyArtifacts plugin only accepts files. Fix for PIP-60.

--- kraken-upload-to-s3-kraken-e2e-logs/include-raw001-kraken-upload-to-s3-kraken-e2e-logs.sh
+++ kraken-upload-to-s3-kraken-e2e-logs/include-raw001-kraken-upload-to-s3-kraken-e2e-logs.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-aws s3 cp --recursive --acl public-read --content-type text/plain --metadata-directive REPLACE ${PROJECT_ARTIFACT} s3://kraken-e2e-logs/${TEST_KIND}/
+aws s3 cp --recursive --acl public-read --content-type text/plain --metadata-directive REPLACE ${PROJECT_ARTIFACT} s3://kraken-e2e-logs/${TEST_KIND}/${PROJECT_ARTIFACT}
