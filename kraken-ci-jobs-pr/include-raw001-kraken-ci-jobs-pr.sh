fbb86bd149bd1ee08d3debe8949a0ab338330e63
Aaron Crickenberger <spiffxp@gmail.com>
2016-05-26 15:50:45 -0700

    add kraken-ci-jobs jjb project

--- kraken-ci-jobs-pr/include-raw001-kraken-ci-jobs-pr.sh
+++ kraken-ci-jobs-pr/include-raw001-kraken-ci-jobs-pr.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-# TODO: this is brittle
-cp /etc/jenkins_jobs/kraken-ci.yaml .
-sed -i.bak -e 's|jobs/job-list.yaml|./job-list.yaml|' kraken-ci.yaml
-
 git checkout origin/master
 jenkins-jobs test --recursive . > jjb_output.master
 
