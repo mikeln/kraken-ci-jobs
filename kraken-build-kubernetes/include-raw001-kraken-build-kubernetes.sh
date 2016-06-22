c94945a37ed88528bbdf6273fd74261548621696
Marat G <maratoid@gmail.com>
2016-06-22 15:33:47 -0700

    make sure built kubernetes tests are cleaned up on cluster destruction

--- kraken-build-kubernetes/include-raw001-kraken-build-kubernetes.sh
+++ kraken-build-kubernetes/include-raw001-kraken-build-kubernetes.sh
@@ -1,6 +1,5 @@
 # build the tests
 cd /var/lib/docker/gobuild
-rm -rf ${KUBE_TESTS_DIR}
 git clone -b ${KUBE_TESTS_BRANCH} ${KUBE_TESTS_REPO} ./${KUBE_TESTS_DIR}
 cd ${KUBE_TESTS_DIR}
 KUBE_SKIP_CONFIRMATIONS=y build/run.sh hack/build-go.sh
