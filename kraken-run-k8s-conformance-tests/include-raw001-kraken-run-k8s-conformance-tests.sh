b4ed1c79ab88d9c3f7bfd05bfaa9cf6e65f5e177
Aaron Crickenberger <spiffxp@gmail.com>
2016-08-09 21:04:24 -0700

    additional artifacts for conformance and density
    trying to match what kubernetes ci stores in gcs
    
    - conformance was exiting before log collection
    - density and conformance weren't placing junit results in the artifacts
      dir that gets archived downstream

--- kraken-run-k8s-conformance-tests/include-raw001-kraken-run-k8s-conformance-tests.sh
+++ kraken-run-k8s-conformance-tests/include-raw001-kraken-run-k8s-conformance-tests.sh
@@ -2,28 +2,30 @@
 
 KRAKEN_ROOT=${KRAKEN_ROOT:-${WORKSPACE}}
 
+## setup
+
+# setup output dir
+OUTPUT_DIR="${KRAKEN_ROOT}/kraken_${GIT_COMMIT}"
+mkdir -p "${OUTPUT_DIR}/artifacts"
+
+# ensure we have access to a kraken cluster
 ${WORKSPACE}/bin/kraken-connect.sh \
   --clustername "${KRAKEN_CLUSTER_NAME}" \
   --clustertype aws \
     --dmname "${PIPELET_DOCKERMACHINE}" \
     --dmshell bash
-    
+
+## run
 export KUBE_ROOT=/var/lib/docker/gobuild/${KUBE_TESTS_DIR}
 export KUBE_CONFORMANCE_KUBECONFIG=${WORKSPACE}/bin/clusters/${KRAKEN_CLUSTER_NAME}/kube_config
-export KUBE_CONFORMANCE_OUTPUT_DIR=${WORKSPACE}/output/conformance
-
-export KUBE_SSH_USER="core"
-export KUBE_SSH_KEY="${KRAKEN_CLUSTER_DIR}/id_rsa"
-
-# setup logging
-KUBE_CONFORMANCE_LOG_DIR=${KRAKEN_ROOT}/kraken_${GIT_COMMIT}/
-KUBE_CONFORMANCE_LOG=${KUBE_CONFORMANCE_LOG_DIR}/build-log.txt
-mkdir -p ${KUBE_CONFORMANCE_LOG_DIR}
+export KUBE_CONFORMANCE_OUTPUT_DIR=${OUTPUT_DIR}/artifacts
 
 # TODO: unclear what part of k8s scripts require USER to be set
-KUBERNETES_PROVIDER=aws USER=jenkins ./hack/parallel-conformance.sh ${KUBE_ROOT} | tee ${KUBE_CONFORMANCE_LOG}
+KUBERNETES_PROVIDER=aws USER=jenkins ./hack/parallel-conformance.sh ${KUBE_ROOT} | tee ${OUTPUT_DIR}/build-log.txt
 # tee isn't exiting >0 as expected, so use the exit status of the script directly
-exit ${PIPESTATUS[0]}
+conformance_result=${PIPESTATUS[0]}
 
 # save logs
-${KRAKEN_ROOT}/hack/log-dump.sh --clustername ${KRAKEN_CLUSTER_NAME} --log-directory ${KUBE_CONFORMANCE_LOG_DIR}/artifacts
+${KRAKEN_ROOT}/hack/log-dump.sh --clustername ${KRAKEN_CLUSTER_NAME} --log-directory ${OUTPUT_DIR}/artifacts
+
+exit ${conformance_result}
