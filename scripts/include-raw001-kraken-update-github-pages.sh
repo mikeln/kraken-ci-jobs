a14a66d7b36faed5fad597f802060fcc1f190b1d
Aaron Crickenberger <spiffxp@gmail.com>
2016-08-31 18:58:50 -0700

    add kraken-e2e-aws-{conformance,density} jobs
    pull out inject-jenkins-build-finished
    invoke upload-to-s3-builder directly
    alter kraken-update-github-pages-builder to use git directly
    invoke kraken-update-github-pages-builder directly
    reorder so scripts run before plugins
    ensure e2e.go is run from a dir with 'kuberentes' in its path
    redo kraken-run-k8s-{conformance,density}
    put kraken-e2e-aws-k8s-conformance results in their own bucket
    go back to using git_commit instead of build_tag for kraken_commit
    remove update-github-pages job, use job_name instead of test_kind
    update kraken-e2e-aws-density to default to '3 30'

--- scripts/include-raw001-kraken-update-github-pages.sh
+++ scripts/include-raw001-kraken-update-github-pages.sh
@@ -2,25 +2,37 @@
 POSTNAME=$(date +"%Y-%m-%d-Job-${KRAKEN_COMMIT}.md")
 TIMESTAMP=$(date +"%Y-%m-%d %H:%M:%S")
 
-mkdir -p ${WORKSPACE}/_posts
+TEST_RESULT="Failure!"
+if [[ "${JENKINS_BUILD_FINISHED:-}" = "SUCCESS" ]]; then
+  TEST_RESULT="Success!"
+fi
 
-cat > ${WORKSPACE}/_posts/${POSTNAME} <<EOF
+pushd "${WORKSPACE}"
+original_branch=$(git rev-parse HEAD)
+git checkout -b gh-pages origin/gh-pages
+
+mkdir -p _posts
+
+cat > _posts/${POSTNAME} <<EOF
 ---
 layout:     post
-title:      ${TEST_KIND} commit/build tag ${KRAKEN_COMMIT}
+title:      ${JOB_NAME} commit/build tag ${KRAKEN_COMMIT}
 date:       ${TIMESTAMP}
-summary:    ${TEST_KIND} test log for commit/build tag ${KRAKEN_COMMIT}.
+summary:    ${JOB_NAME} test log for commit/build tag ${KRAKEN_COMMIT}.
 category:   jekyll
 ---
 
 ${TEST_RESULT}
 
-- ${TEST_KIND} test log: [Log File](${LOG_LINK})
+- ${JOB_NAME} test log: [Log File](${LOG_LINK})
 EOF
 
 git config user.email "maratoid+samsungjenkins@gmail.com"
-git config user.name "Samsung Jenkins"
+git config user.name "Samsung Jenkins (${KRAKEN_CI_NAME})"
 
-git add .
-git add -u
-git commit -m "Auto-update site with Kraken SHA ${KRAKEN_COMMIT}"
+git add _posts/${POSTNAME}
+git commit -m "${JOB_NAME} result: ${TEST_RESULT} kraken_sha: ${KRAKEN_COMMIT}"
+
+git push origin gh-pages:gh-pages
+git checkout "${original_branch}"
+popd
