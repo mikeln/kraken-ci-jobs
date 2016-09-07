d63a43b05e1cda3899d013d41c99b9806ee955c7
Aaron Crickenberger <spiffxp@gmail.com>
2016-09-07 13:28:33 -0700

    use unique postnames and rebase pushes to gh-pages branch

--- scripts/include-raw001-kraken-update-github-pages.sh
+++ scripts/include-raw001-kraken-update-github-pages.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-POSTNAME=$(date +"%Y-%m-%d-Job-${KRAKEN_COMMIT}.md")
+POSTNAME=$(date +"%Y-%m-%d.%H%M%S.${JOB_NAME}-${BUILD_NUMBER}.md")
 TIMESTAMP=$(date +"%Y-%m-%d %H:%M:%S")
 
 TEST_RESULT="Failure!"
@@ -33,6 +33,16 @@ git config user.name "Samsung Jenkins (${KRAKEN_CI_NAME})"
 git add _posts/${POSTNAME}
 git commit -m "${JOB_NAME} result: ${TEST_RESULT} kraken_sha: ${KRAKEN_COMMIT}"
 
-git push origin gh-pages:gh-pages
-git checkout "${original_branch}"
+attempts=5
+while ! git push origin gh-pages:gh-pages; do
+  git pull --rebase origin gh-pages
+  if [ ${attempts} -gt 0 ]; then
+    attempts=$((attempts-1))
+    sleep 1
+  else
+    break
+  fi
+done
+
 popd
+git checkout "${original_branch}"
