bed4e52337efa66c0a2db01afe68ea755456d299
Marat Garafutdinov <maratoid@gmail.com>
2016-10-24 16:16:29 -0700

    more fixes to paths, typos (#141)
    * fix naming errors
    
    * fix naming errors
    
    * more fixes to paths, typos

--- scripts/include-raw001-k2-build-cluster-gke.sh
+++ scripts/include-raw001-k2-build-cluster-gke.sh
@@ -3,7 +3,7 @@ aws s3 cp s3://sundry-automata/keys/atlas.cnct.io/atlas.cnct.io.key.json ${JENKI
 
 # start k2
 docker run -e "K2_CLUSTER_NAME=${K2_CLUSTER_NAME}" \
-  -e "JENKINS_HOME=${JENKINS_HOME}"
+  -e "JENKINS_HOME=${JENKINS_HOME}" \
   --volumes-from=jenkins \
   ${K2_CONTAINER_IMAGE} \
   ./up.sh --output ${WORKSPACE}/${K2_CLUSTER_NAME} --config ${WORKSPACE}/${K2_CLUSTER_NAME}/${K2_CLUSTER_NAME}.yaml
--- scripts/include-raw001-k2-get-cluster-config.sh
+++ scripts/include-raw001-k2-get-cluster-config.sh
@@ -1,4 +1,6 @@
 # create a config file
+mkdir -p ${WORKSPACE}/${K2_CLUSTER_NAME}
+
 if [ -z "$K2_CONFIG_URI" ]; then
   echo ${K2_CONFIG_TEXT} > ${WORKSPACE}/${K2_CLUSTER_NAME}/${K2_CLUSTER_NAME}.yaml
 else
