07e34e721d3e5a0a17fb68f129605241064b08dd
Marat G <maratoid@gmail.com>
2016-10-25 16:39:56 -0700

    fix environment variable names for gce keys

--- scripts/include-raw001-k2-charts-postcommit.sh
+++ scripts/include-raw001-k2-charts-postcommit.sh
@@ -1,5 +1,5 @@
 # make all
 docker run --volumes-from=jenkins quay.io/samsung_cnct/k2:latest bash -c \
-  "gcloud auth activate-service-account ${GCE_PROD_SERVICE_ACCOUNT_ID} --key-file /gcloud/prod-service-account.json; \
+  "gcloud auth activate-service-account ${KRAKEN_GCE_PROD_SERVICE_ACCOUNT_ID} --key-file /gcloud/prod-service-account.json; \
     cd ${WORKSPACE}; \
       make all"
